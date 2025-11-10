96322c8b6e5220d41e481f2ab93ea6b44aea1972
Jonathan Pérez <t.jspg2@gmail.com>
2025-11-09 21:10:22 -0500

    chore(vite): remove host

--- just-pay-client/vite.config.ts
+++ just-pay-client/vite.config.ts
@@ -6,10 +6,6 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
