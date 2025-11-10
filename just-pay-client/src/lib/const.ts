8e5f2363533e9902cbd34e619b93c9951fe6f99e
Jonathan Pérez <t.jspg2@gmail.com>
2025-11-09 20:26:49 -0500

    feat(client): add consts with redirect when success and cancel checkout

--- just-pay-client/src/lib/const.ts
+++ just-pay-client/src/lib/const.ts
@@ -1,2 +1,2 @@
-export const VITE_SUCCESS_CHECKOUT_URL = window.location.origin + '/subscriptions/success';
+export const VITE_SUCCESS_CHECKOUT_URL = window.location.origin + '/user-panel?planUpdated=true';
 export const VITE_CANCEL_CHECKOUT_URL = window.location.origin + '/user-panel';
